8b00bde706faba1f63595a3c1d3dbb6925f79d20
jimeiti <63957383+jimeiti@users.noreply.github.com>
2020-06-08 22:12:04 -0300

    add htmlcss#15

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $('#nav-toggle').click(function(e){
+            e.preventDefault();
+            $(this).toggleClass('active');
+            $('.header-collapse').toggleClass('active');
+    });
+
+    // Carousel Principal 
+
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        lazyLoad:true,
+        loop:true,
+        margin:10
+    });
+});
+
